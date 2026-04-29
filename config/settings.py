cad40101a84fd1f686d21b362719c78c27095a34
Avazbek Mexriddinov <avazbekmexriddinov63@gmail.com>
2026-04-29 21:14:43 +0500

    Added CSRF trusted origins for Render

--- config/settings.py
+++ config/settings.py
@@ -118,3 +118,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CSRF_TRUSTED_ORIGINS = ['https://studentfolio.onrender.com']
