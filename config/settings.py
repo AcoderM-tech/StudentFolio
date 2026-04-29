7884d96056f3504e8d64414fd1056e582905de4e
Avazbek Mexriddinov <avazbekmexriddinov63@gmail.com>
2026-04-29 21:06:52 +0500

    Settings va requirements yangilandi

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,7 @@ AUTH_USER_MODEL = 'mainapp.Users'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
