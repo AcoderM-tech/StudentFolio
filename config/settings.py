fa36eedc5ef598af2533526a3733e5c3029b653a
AcoderM <242905891+AcoderM-tech@users.noreply.github.com>
2026-05-16 19:34:03 +0500

    Update AWS S3 settings in settings.py

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# ── Media fayllar: Umumiy sozlamalar (Urls.py muammosiz topishi uchun) ──────
 # ── Media fayllar: Umumiy sozlamalar ────────────────────────────────────────
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -90,12 +89,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AWS_ACCESS_KEY_ID = os.environ.get('SUPABASE_S3_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = os.environ.get('SUPABASE_S3_SECRET_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('SUPABASE_BUCKET')
-AWS_S3_ENDPOINT_URL = os.environ.get('SUPABASE_S3_ENDPOINT')
 
-# Shartni olib tashlab, majburlab S3 storageni ishlatamiz
+# Diqqat: AWS_S3_ENDPOINT_URL o'rniga to'g'ridan-to'g'ri unga mos parametrlar
+AWS_S3_ENDPOINT_URL = os.environ.get('SUPABASE_S3_ENDPOINT')
+AWS_S3_CUSTOM_DOMAIN = None  # Supabase havolalari buzilib ketmasligi uchun
+
+# Majburlab S3 storageni ishlatamiz
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = None  
 CSRF_TRUSTED_ORIGINS = ['https://studentfolio.onrender.com']
 
 if not DEBUG:
